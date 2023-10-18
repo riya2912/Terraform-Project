c6ac4336bdd729e415738dbf183ac2002dc4c637
riya2912 <riyapatel2912@gmail.com>
2023-10-18 07:48:40 +0100

    Modify the configuration file

--- modules/VPC/main.tf
+++ modules/VPC/main.tf
@@ -7,13 +7,6 @@ resource "aws_vpc" "my_vpc" {
   }
 }
 
-resource "aws_internet_gateway" "internet_gateway" {
-  vpc_id = aws_vpc.my_vpc.id
-  tags = {
-    Name = "Internet Gateway"
-  }
-}
-
 resource "aws_subnet" "public_subnet" {
   count         = length(var.availability_zones)
   vpc_id        = aws_vpc.my_vpc.id
@@ -35,12 +28,23 @@ resource "aws_subnet" "private_subnets" {
   }
 }
 
+resource "aws_internet_gateway" "internet_gateway" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "Internet Gateway"
+  }
+}
+
 resource "aws_route_table" "public_route_tables" {
   count = length(aws_subnet.public_subnet)
   vpc_id = aws_vpc.my_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.internet_gateway.id
+  }
   tags = {
     Name = "Public Route Table"
-  }
+  }  
 }
 
 resource "aws_route_table" "private_route_tables" {
@@ -51,4 +55,14 @@ resource "aws_route_table" "private_route_tables" {
   }
 }
 
+resource "aws_route_table_association" "publicRTassociation" {
+  count           = length(aws_subnet.public_subnet)
+  subnet_id       = aws_subnet.public_subnet[count.index].id
+  route_table_id  = aws_route_table.public_route_tables[count.index].id
+}
 
+resource "aws_route_table_association" "privateRTassociation" {
+  count           = length(aws_subnet.private_subnets)
+  subnet_id       = aws_subnet.private_subnets[count.index].id
+  route_table_id  = aws_route_table.private_route_tables[count.index].id
+}
